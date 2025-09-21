3266cc22c1160e35f7be36342bf398243653bf30
KumaraswamyBakkashetti <kumaraswamybakkashetti@gmail.com>
2025-09-21 10:32:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,5 +19,9 @@ connectDB();
 app.use("/api/auth", authRoutes);
 
 // Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => 
+  {
+    console.log("Hello ");
+    console.log(`🚀 Server has started running on port ${PORT}`)
+  });
